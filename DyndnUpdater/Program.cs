cf85f40babde80188a71b7ee68694d4a35ca338e
Tzuchin Chang <soechin@gmail.com>
2018-03-21 13:53:17 +0800

    Change updator work flow.

--- DyndnUpdater/Program.cs
+++ DyndnUpdater/Program.cs
@@ -16,12 +16,16 @@ namespace DyndnUpdater
         public static DyndnClient Client { get; set; }
         public static string Xml { get; set; }
         public static XmlDocument Config { get; set; }
+        public static bool Exiting { get; set; }
+        public static ManualResetEvent ExitEvent { get; set; }
 
         public static void Main(string[] args)
         {
             Client = new DyndnClient();
             Xml = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TwnicDyndn.xml");
             Config = new XmlDocument();
+            Exiting = false;
+            ExitEvent = new ManualResetEvent(false);
 
             try
             {
@@ -53,7 +57,49 @@ namespace DyndnUpdater
                 },
             };
 
-            Client.Query();
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                Exiting = true;
+                ExitEvent.Set();
+
+                e.Cancel = true;
+            };
+
+            while (!Exiting)
+            {
+                if (!Client.Query())
+                {
+                    if (Client.LastQuery && !Exiting)
+                    {
+                        if (ExitEvent.WaitOne(TimeSpan.FromSeconds(Client.LoginInterval)))
+                        {
+                            break;
+                        }
+                    }
+
+                    continue;
+                }
+
+                do
+                {
+                    if (!Client.Login())
+                    {
+                        continue;
+                    }
+
+                    do
+                    {
+                        if (ExitEvent.WaitOne(TimeSpan.FromSeconds(Client.LoginInterval)))
+                        {
+                            break;
+                        }
+                    }
+                    while (Client.Update());
+
+                    Client.Logout();
+                }
+                while (!Client.LastLogin && !Exiting);
+            }
         }
     }
 
@@ -74,13 +120,25 @@ namespace DyndnUpdater
         [DllImport("kernel32.dll")]
         private static extern int GetSystemDefaultLangID();
 
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public DyndnServer[] Servers { get; set; }
+        private string _username = null;
+        private string _password = null;
+        private DyndnServer[] _servers = null;
+        private int _serverIndex = -1;
+        private TcpClient _tcpClient = null;
+        private UdpClient _udpClient = null;
+        private DyndnServer[] _loginServers = null;
+        private int _loginServerIndex = -1;
+        private int _loginInterval = 0;
+        private int _loginSession = 0;
+        private bool _logoutSuccess = false;
+        private bool _updateSuccess = false;
 
-        private DyndnServer[] _nameServers;
-        private TcpClient _tcpClient;
-        private UdpClient _udpClient;
+        public string Username { set => _username = value; }
+        public string Password { set => _password = value; }
+        public DyndnServer[] Servers { set => _servers = value; }
+        public int LoginInterval { get => _loginInterval; }
+        public bool LastQuery { get => (_serverIndex >= (_servers.Length - 1)); }
+        public bool LastLogin { get => (_loginServerIndex >= (_loginServers.Length - 1)); }
 
         private bool Open(DyndnServer server)
         {
@@ -94,7 +152,7 @@ namespace DyndnUpdater
                 {
                     result = _tcpClient.BeginConnect(server.Host, server.Port, null, null);
 
-                    if (result.AsyncWaitHandle.WaitOne(3000))
+                    if (result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(3)))
                     {
                         _tcpClient.EndConnect(result);
                         return true;
@@ -164,7 +222,7 @@ namespace DyndnUpdater
                         buffer = new byte[256];
                         result = stream.BeginRead(buffer, 0, buffer.Length, null, null);
 
-                        if (result.AsyncWaitHandle.WaitOne(3000))
+                        if (result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(3)))
                         {
                             length = stream.EndRead(result);
                             return Encoding.Default.GetString(buffer, 0, length);
@@ -187,7 +245,7 @@ namespace DyndnUpdater
                     endpoint = null;
                     result = _udpClient.BeginReceive(null, null);
 
-                    if (result.AsyncWaitHandle.WaitOne(3000))
+                    if (result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(3)))
                     {
                         buffer = _udpClient.EndReceive(result, ref endpoint);
                         return Encoding.Default.GetString(buffer);
@@ -207,14 +265,25 @@ namespace DyndnUpdater
             string[] lines, tokens;
             List<DyndnServer> servers;
 
+            if (!response.EndsWith("\r\n"))
+            {
+                return;
+            }
+
             lines = response.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
             servers = new List<DyndnServer>();
+
+            Console.WriteLine("收到回應 {0}", string.Join(", ", lines));
 
             foreach (string line in lines)
             {
                 tokens = line.Split(new string[] { " " }, StringSplitOptions.None);
 
-                if (tokens[0] == "201")
+                if (tokens[0] == "200") // 登出成功
+                {
+                    _logoutSuccess = true;
+                }
+                else if (tokens[0] == "201") // 伺服器資訊
                 {
                     if (tokens[3] == "tcp")
                     {
@@ -235,37 +304,148 @@ namespace DyndnUpdater
                         });
                     }
                 }
-                else if (tokens[0] == "202")
+                else if (tokens[0] == "205") // 更新間隔
                 {
+                    _loginInterval = int.Parse(tokens[1]);
+                    _updateSuccess = true;
+                }
+                else if (tokens[0] == "208") // 更新速度太快
+                {
+                    _updateSuccess = true;
+                }
+                else if (tokens[0] == "209") // 會話編號
+                {
+                    _loginSession = int.Parse(tokens[1]);
+                }
+                else
+                {
+                    Console.WriteLine("非預期的回應 {0}", tokens[0]);
                 }
             }
 
             if (servers.Count > 0)
             {
-                _nameServers = servers.ToArray();
+                _loginServers = servers.ToArray();
+            }
+
+            if (_loginInterval < 30)
+            {
+                _loginInterval = 30;
             }
         }
 
-        public void Query()
+        public bool Query()
         {
             string request, response;
+            DyndnServer server;
 
-            request = string.Format("102 {0} {1}\r\n", Username, Password);
+            _serverIndex = (_serverIndex + 1) % _servers.Length;
+            _loginServerIndex = -1;
+            _loginServers = null;
 
-            foreach (DyndnServer server in Servers)
+            request = string.Format("102 {0} {1}\r\n", _username, _password);
+            server = _servers[_serverIndex];
+
+            Console.WriteLine("送出請求 102 {0}", server);
+
+            if (Open(server))
             {
-                if (Open(server))
+                response = Send(request);
+
+                if (response != null)
                 {
-                    response = Send(request);
-
-                    if (response != null)
-                    {
-                        Parse(response);
-                    }
-
-                    Close();
+                    Parse(response);
                 }
+
+                Close();
             }
+
+            return (_loginServers != null);
+        }
+
+        public bool Login()
+        {
+            string request, response;
+            DyndnServer server;
+
+            _loginServerIndex = (_loginServerIndex + 1) % _loginServers.Length;
+            _loginInterval = 0;
+            _loginSession = 0;
+
+            request = string.Format("101 150 {0} {1}.{2} {3} {4}\r\n",
+                GetSystemDefaultLangID(), Environment.OSVersion.Version.Major,
+                Environment.OSVersion.Version.Minor, _username, _password);
+            server = _loginServers[_loginServerIndex];
+
+            Console.WriteLine("送出請求 101 {0}", server);
+
+            if (Open(server))
+            {
+                response = Send(request);
+
+                if (response != null)
+                {
+                    Parse(response);
+                }
+
+                Close();
+            }
+
+            return (_loginSession > 0);
+        }
+
+        public bool Logout()
+        {
+            string request, response;
+            DyndnServer server;
+
+            _logoutSuccess = false;
+
+            request = string.Format("104 {0}\r\n", _username);
+            server = _loginServers[_loginServerIndex];
+
+            Console.WriteLine("送出請求 104 {0}", server);
+
+            if (Open(server))
+            {
+                response = Send(request);
+
+                if (response != null)
+                {
+                    Parse(response);
+                }
+
+                Close();
+            }
+
+            return _logoutSuccess;
+        }
+
+        public bool Update()
+        {
+            string request, response;
+            DyndnServer server;
+
+            _updateSuccess = false;
+
+            request = string.Format("103 {0} {1}\r\n", _loginSession, _username);
+            server = _loginServers[_loginServerIndex];
+
+            Console.WriteLine("送出請求 103 {0}", server);
+
+            if (Open(server))
+            {
+                response = Send(request);
+
+                if (response != null)
+                {
+                    Parse(response);
+                }
+
+                Close();
+            }
+
+            return _updateSuccess;
         }
     }
 }
